a00d3764128b7205bd14d7d0d338486868b80094
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2024-02-19 20:30:25 -0300

    Adicionar arquivos de projeto.

---
+++ leetcode_ContainsDuplicate/Program.cs
@@ -0,0 +1,22 @@
+﻿bool ContainsDuplicate(int[] nums)
+{
+    Dictionary<int, int> quantValores = new Dictionary<int, int>();
+    int aux = 0;
+
+    for (int i=0; i<nums.Length; i++)
+    {
+        aux = nums[i];
+        for (int j=i+1; j<nums.Length;j++)
+        {
+            if (nums[j] == aux)
+            {
+                return true;
+            }
+        }
+    }
+        return false;
+}
+
+int[] arr = { 1, 2, 3, 4, 2, 6 };
+
+Console.WriteLine(ContainsDuplicate(arr));
